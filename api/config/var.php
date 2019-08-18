7477774cb134c6764503a0b7a68ee476f792e8bb
Komche <31341079+Komche@users.noreply.github.com>
2019-08-18 21:44:41 +0200

    ajout de mon api

---
+++ api/config/var.php
@@ -0,0 +1,6 @@
+<?php
+        //Pour la connexion à la base de donnée
+        $this->config["host"] = 'localhost';
+        $this->config["db_name"] = 'saroapp';
+        $this->config["username"] = 'root';
+        $this->config["password_"] = '';
