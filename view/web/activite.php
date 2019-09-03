55c5093b5a76a0e265f3ddbf232764e1c563849a
227Mahaman <zalkiabass.456@gmail.com>
2019-09-03 14:14:22 +0100

    Ajout pagination

--- view/web/activite.php
+++ view/web/activite.php
@@ -1,17 +1,30 @@
 <?php 
     $title = "AEMN| Nos activités";
     ob_start();
+    $currentPage = (int)($_GET['page'] ?? 1);
+    if($currentPage <= 0){
+        throw new Exception("Numéro de page invalide");
+        
+    }
+    $count = countArticles(7);
+    $perPage = 6;
+    $pages = ceil($count / $perPage);
+    if ($currentPage > $pages){
+        throw new Exception("Cette page n'existe pas");
+    }
+    $offset = $perPage * ($currentPage - 1);
+    $link = "index.php?action=activite";
 ?>
 
 <main>
-<div class="article">
+    <div class="article">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-12 col-md-8">
                 <div class="container">
-                <h1 align="center">Nos Annonces</h1>
+                <h3 align="center">Nos Annonces</h3>
                     <div class="row">
                         <?php
-                        $datas = getArticles(null, 7);
+                        $datas = getArticles(null, 7, $perPage, $offset);
                         foreach ($datas as $data)
                         {
                             ?>
@@ -29,11 +42,21 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <h1>ok ok ok</h1>
-                <!--<iframe title="La page Facebook de l'AEMN" src="https://www.facebook.com/Associations-des-Etudiants-Musulmans-du-Niger-AEMN-262470803769982" width="400px" height="400px" frameborder="0" style="border:0" allowfullscreen></iframe>-->
-                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpg%2FAssociations-des-Etudiants-Musulmans-du-Niger-AEMN-262470803769982%2Fposts%2F%3Fref%3Dpage_internal&tabs=timeline&width=400&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="400" height="150" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+            <div class="col-12 col-md-4">
+                <h3>Notre page <i class="fa fa-facebook"></i>acebook</h3>
+                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpg%2FAssociations-des-Etudiants-Musulmans-du-Niger-AEMN-262470803769982%2Fposts%2F%3Fref%3Dpage_internal&tabs=timeline&width=400&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="400" height="150" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
             </div>
+        </div>
+        <div class="d-flex justify-content-between my-4">
+            <?php if($currentPage > 1):?>
+                <?php
+                if($currentPage > 2) $link .= '&page' . ($currentPage - 1);
+                ?>
+                    <a href="<?= $link ?>" class="btn btn-primary">&laquo; Page précédente</a>
+                <?php endif; ?>
+            <?php if($currentPage < $pages):?>
+                <a href="index.php?action=activite&page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page suivante &raquo;</a>
+            <?php endif; ?>
         </div>
     </div>
 </main>
