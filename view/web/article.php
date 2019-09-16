d62c085a360a165803659e1f554619c40353567c
227Mahaman <zalkiabass.456@gmail.com>
2019-09-16 23:39:16 +0100

    Réorganisation du fichier article.php

--- view/web/article.php
+++ view/web/article.php
@@ -4,37 +4,33 @@
 ?>
 
 <main>
-    <div style="margin-top: 100px" class="article">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="container">
-                    <?php 
-                      $datas = Article($id);
-                      foreach ($datas as $data)
-                      {
-                          ?>
-                          <div class="card " style="width: 45rem;">
-                              <img class="card-img-top" src="<?= $data['url'] ?>" alt="Card image cap">
-                              <div class="card-body">
-                                  <h5 class="card-title"><?= $data['title'] ?></h5>
-                                  <p class="card-text"> <?= $data['content'] ?></p>
-                              </div>
-                              <ul class="list-group list-group-flush">
-                                  <li class="list-group-item">Rédiger par : <?= $data['last_name'].' '.$data['first_name'] ?>
-                                      <span style="margin-left: 150px; font-weight: bold"><?= $data['likes'].' ' ?></span>
-                                      <a class="" href="index.php?like=<?= $like ?>&amp;article=<?= $data['id_article'] ?>""><i style="color:red" class="fa fa-heart"></i></a>
-                                  </li>
-                                  <li class="list-group-item">
-                                      Le <?= strftime(' %A %d %B  %G ',strtotime($data['dates'])) ?>
-                                  </li>
-                              </ul>
-                          </div>
-                          <br>
-                          <?php
-                        }
+<br><br><br><br>
+    <div class="container">
+        <div class="row ">
+            <?php
+                $datas = Article($id);
+                foreach ($datas as $data)
+                {
                     ?>
-                </div>
-            </div>
+                    <div class="col-lg-4 col-md-8 portfolio-item filter-app wow fadeInUp">
+                        <div class="portfolio-wrap">
+                            <figure>
+                                <img src="<?= $data['url'] ?>" class="img-fluid" alt="<?= $data['title'] ?>">
+                                <a href="<?= $data['url'] ?>" data-lightbox="portfolio" data-title="<?= $data['title'] ?>" class="link-preview" title="Afficher"><i class="ion ion-eye"></i></a>
+                                <a href="#" class="link-details" title="Plus de Détails"><i class="ion ion-android-open"></i></a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-4">
+                        <h1><?= $data['title'] ?></h1>
+                        <div class="portfolio-info">
+                            <p>Le <?= strftime(' %A %d %B  %G ',strtotime($data['dates'])) ?></p>
+                            <p class="card-text"> <?= $data['content'] ?></p>
+                        </div>
+                    </div>
+                    <?php
+                }
+            ?>
         </div>
     </div>
 </main>
